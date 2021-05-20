9e3532bfbe0feac61b95a83c7d624ad0120608b2
JB Profile <cool508@naver.com>
2021-05-20 15:05:02 +0900

    예제 4.14 commit

--- app/App.js
+++ app/App.js
@@ -26,25 +26,25 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   cardContainer: { // 프로필카드에 border 속성 추가
-    alignItems: 'center',    
+    alignItems: 'center', // 프로필 카드를 수평축에서 중앙으로 정렬
     borderColor: 'black',
     borderWidth: 3,
-    borderStyle: 'solid',
+    borderStyle: 'solid', // 사용자의 이미지를 수평축에서 중앙으로 정렬
     borderRadius: 20,
     backgroundColor: profileCardColor,
     width: 300,
     height: 400
   },
-  cardImageContainer: { // 이미지 컨테이너(image contanier)는 120x120 크기의 정사각형. borderRadius 속성을 60(120의 반)으로 지정해서 원으로 나타냄
-    alignItems: 'center',    
+  cardImageContainer: {   // 이미지 컨테이너(image contanier)는 120x120 크기의 정사각형. borderRadius 속성을 60(120의 반)으로 지정해서 원으로 나타냄
+    alignItems: 'center', // 사용자의 이미지를 수평축에서 중앙으로 정렬
     backgroundColor: 'white',
     borderWidth: 3,
     borderColor: 'black',
     width: 120,
     height: 120,
     borderRadius: 60,
-    marginTop: 30,    
-    paddingTop: 15    
+    marginTop: 30,  // 프로필 카드와 원의 상단의 간격
+    paddingTop: 15  // 원과 안쪽 이미지 사이의 간격
   },
   cardImage: {  // 이미지에 적용한 스타일
       width: 80,
